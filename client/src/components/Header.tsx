c69ab7e504cf37c2e0af0989baf8cd49a6d4d9a0
Firas YAHYAOUI <fyrasdjabou.el19@gmail.com>
2022-07-08 12:35:59 +0100

    This should break it

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <a className='navbar-brand' href='/'>
                 <div className='d-flex'>
                     <img src={Logo} alt='logo' className='mr-2' />
-                    <div>ProjectMgmt</div>
+                    <div>ProjectManagement</div>
                 </div>
             </a>
         </div>
